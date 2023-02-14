33235cc19ab1eb475374cace5beae0450d0fdaaa
Ryan-Knecht <ryank16@byu.edu>
2023-02-13 19:22:56 -0700

    Add project files.

---
+++ Mission06/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext blahContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NewMovies()
+        {
+            return View("NewMovies");
+        }
+
+        [HttpPost]
+        public IActionResult NewMovies(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                blahContext.Add(m);
+                blahContext.SaveChanges();
+                return View("Confirmation", m);
+            }
+            else
+            {
+                return View(m);
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06/Models/MovieContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06.Models
+{
+    public class MovieContext : DbContext
+    {
+        // Constructor
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<Movie> movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieID = 1,
+                    Category = "Adventure",
+                    Title = "Hook",
+                    Year = 1991,
+                    Director = "Steven Spielberg",
+                    Rating = "PG"
+                },
+                new Movie
+                {
+                    MovieID = 2,
+                    Category = "Comedy",
+                    Title = "School of Rock",
+                    Year = 2003,
+                    Director = "Richard Linklater",
+                    Rating = "PG-13"
+                },
+                new Movie
+                {
+                    MovieID = 3,
+                    Category = "Action",
+                    Title = "Scott Pilgrim vs. the World",
+                    Year = 2010,
+                    Director = "Edgar Wright",
+                    Rating = "PG-13"
+                }
+            );
+        }
+    }
+}
